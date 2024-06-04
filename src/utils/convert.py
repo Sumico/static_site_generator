c2597d7d18c5d051ff06777586f86ddaac1b34f9
Neill Killgore <nkillgore@gmail.com>
2024-06-04 11:37:07 -0500

    refactor converter to remove else statements. add error message for invalid type

--- src/utils/convert.py
+++ src/utils/convert.py
@@ -4,15 +4,14 @@ from htmlnode import LeafNode
 def text_node_to_html_node(node):
     if node.text_type == "bold":
         return LeafNode("b", node.text)
-    elif node.text_type == "text":
+    if node.text_type == "text":
         return LeafNode(None, node.text)
-    elif node.text_type == "italics":
+    if node.text_type == "italics":
         return LeafNode("i", node.text)
-    elif node.text_type == "link":
+    if node.text_type == "link":
         return LeafNode("a", node.text, {"href": node.url})
-    elif node.text_type == "code":
+    if node.text_type == "code":
         return LeafNode("code", node.text)
-    elif node.text_type == "image":
+    if node.text_type == "image":
         return LeafNode("img", None, {"src": node.url, "alt": node.text})
-    else:
-        raise Exception
+    raise ValueError("Invalid text type: {node.text_type}")
